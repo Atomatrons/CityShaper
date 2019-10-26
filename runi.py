1bb1176494984fec4c1711e60417f46ce17768f0
Kyan <kyan@bookmarked.us>
2019-10-26 15:00:43 -0400

    fixed and commented runi and runi2.

--- runi.py
+++ runi.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env micropython
 import Robot
+"""This is mission 12 and 13 and it scores 95 points. This is Kyan and Rushil's mission.
+"""
+
 #Robot.gyro.compass_point = 0
-Robot.tank_pair.on_for_rotations(50, 50, -4)
 Robot.tank_pair.on_for_rotations(50, 50, 4.5)
+Robot.tank_pair.on_for_rotations(50, 50, -4)
--- runi2.py
+++ runi2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env micropython
 import Robot
+""" This is the tan block pushing mission and it scores 60 points. This is Kyan and Rushil's second mission.
+"""
+
 #Robot.gyro.compass_point = 0
-Robot.tank_pair.on_for_rotations(50, 50, -2.75)
 Robot.tank_pair.on_for_rotations(50, 50, 3)
+Robot.tank_pair.on_for_rotations(50, 50, -2.75)
