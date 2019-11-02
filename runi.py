509a97a181a910f2025f666940fc78c82045fa41
Rushil <rparikh022@gmail.com>
2019-11-02 15:56:57 -0400

    Made the robot go to home

--- runi.py
+++ runi.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env micropython
+
 import Robot
+import My_block 
 #Robot.gyro.compass_point = 0
-Robot.tank_pair.on_for_rotations(50, 50, -4)
-Robot.tank_pair.on_for_rotations(50, 50, 4.5)
+Robot.tank_pair.on_for_rotations(50, 50, 2.25)
+Robot.tank_pair.on_for_rotations(50, 50, -1)
+My_block.spin_turn(200) 
+Robot.tank_pair.on_for_rotations(50, 50, 2)
