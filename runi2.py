64d14ed2474ea28128b6a746916ed8bb37b4280b
Kyan <kyan@bookmarked.us>
2019-11-06 18:21:24 -0500

    added code to push_tan_block_return()

--- runi2.py
+++ runi2.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env micropython
 import Robot
-""" This is the tan block pushing mission and it scores 60 points. This is Kyan and Rushil's second mission.
-"""
 
-#Robot.gyro.compass_point = 0
-#Robot.tank_pair.on_for_rotations(50, 50, 4)
-#Robot.tank_pair.on_for_rotations(50, 50, -2.75)
-Robot.tank_pair.on_for_rotations(50, 50, 4.5)
-Robot.tank_pair.on_for_rotations(50, 50, -2.25)
-Robot.tank_pair.on_for_rotations(0, 20, 3)
-Robot.tank_pair.on_for_rotations(50, 50, 5)
+
+def push_tan_block_return():
+    """ 
+    This is the tan block pushing mission and it scores 60 points. This is Kyan and Rushil's second mission.
+    """
+    #Robot.gyro.compass_point = 0
+    #Robot.tank_pair.on_for_rotations(50, 50, 4)
+    #Robot.tank_pair.on_for_rotations(50, 50, -2.75)
+    Robot.tank_pair.on_for_rotations(50, 50, 4.5)
+    Robot.tank_pair.on_for_rotations(50, 50, -2.25)
+    Robot.tank_pair.on_for_rotations(0, 20, 3)
+    Robot.tank_pair.on_for_rotations(50, 50, 5)
