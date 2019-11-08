97195ae957f62e7d22d3eb4e375ade6681587961
Kyan <kyan@bookmarked.us>
2019-11-08 18:56:36 -0500

    New changes for runi and runi2

--- runi.py
+++ runi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env micropython
 
+import My_block
 import Robot
 """
 This is mission 12 and 13 and it scores 95 points. This is Kyan and Rushil's mission.
@@ -9,8 +10,8 @@ def push_tan_blocks_and_return():
     """
     Pushes blocks to the red circle then returns to base.
     """
-    #Robot.gyro.compass_point = 0
+    Robot.gyro.compass_point = 75
     Robot.tank_pair.on_for_rotations(50, 50, 2.9)
     Robot.tank_pair.on_for_rotations(50, 50, -1.5)
-    Robot.tank_pair.on_for_rotations(100, 0, .5)
-    Robot.tank_pair.on_for_rotations(50, 50, 3.5)
+    My_block.spin_turn(90)
+    Robot.tank_pair.on_for_rotations(50, 50, 5)
--- runi2.py
+++ runi2.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env micropython
 import Robot
-
+import My_block
 
 def push_block_return():
     """ 
     This is the tan block pushing mission and it scores 60 points. This is Kyan and Rushil's second mission.
     """
-    #Robot.gyro.compass_point = 0
+    Robot.gyro.compass_point = 75
     #Robot.tank_pair.on_for_rotations(50, 50, 4)
     #Robot.tank_pair.on_for_rotations(50, 50, -2.75)
     Robot.tank_pair.on_for_rotations(50, 50, 4.5)
     Robot.tank_pair.on_for_rotations(50, 50, -2.25)
-    Robot.tank_pair.on_for_rotations(0, 20, 3)
-    Robot.tank_pair.on_for_rotations(50, 50, 5)
+    My_block.spin_turn(90)
+    Robot.tank_pair.on_for_rotations(50, 50, -5)
