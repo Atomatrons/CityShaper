2eafa2ee2f10dc940117e6531753924abd3a7f63
superastro99 <superastro33@gmail.com>
2019-11-08 19:07:08 -0500

    changed Master program

--- Master_Program.py
+++ Master_Program.py
@@ -6,21 +6,25 @@ import Robot
 # Checks if certain buttons are being pressed
 while True:
     if Robot.button.up == True:
-        for int in range(0, 1000):
+        Robot.sound.tone([(500, 350, 50)])
+        for int in range(0, 1400):
             if Robot.button.enter == True:
-                Robot.sound.speak("Run 1 would be running")
+                print("RUN 1 RUN 1 RUN 1 RUN 1 RUN 1 RUN 1 RUN 1")
 
     if Robot.button.down == True:
-        for int in range(0, 1000):
+        Robot.sound.tone([(500, 350, 50), (500, 350, 50)])
+        for int in range(0, 1400):
             if Robot.button.enter == True:
-                Robot.sound.speak("Run 2 would be running")
+                print("RUN 2 RUN 2 RUN 2 RUN 2 RUN 2 RUN 2 RUN 2")
 
     if Robot.button.left == True:
-        for int in range(0, 1000):
+        Robot.sound.tone([(500, 350, 50), (500, 350, 50), (500, 350, 50)])
+        for int in range(0, 1400):
             if Robot.button.enter == True:
-                Robot.sound.speak("Run 3 would be running")
+                print("RUN 3 RUN 3 RUN 3 RUN 3 RUN 3 RUN 3 RUN 3")
 
     if Robot.button.right == True:
-        for int in range(0, 1000):
+        Robot.sound.tone([(500, 350, 50), (500, 350, 50), (500, 350, 50), (500, 350, 50)])
+        for int in range(0, 1400):
             if Robot.button.enter == True:
-                Robot.sound.speak("Run 4 would be running")
+                print("RUN 4 RUN 4 RUN 4 RUN 4 RUN 4 RUN 4 RUN 4")
