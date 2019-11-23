cf363b6d162d5dd4043fa8afbf3727b464c39561
Rushil <rparikh022@gmail.com>
2019-11-23 16:08:08 -0500

    Same as before

--- elevator.py
+++ elevator.py
@@ -3,9 +3,10 @@ import Robot
 
 import My_block
 
-Robot.gyro.compass_point = 80
+Robot.gyro.compass_point = 81
 My_block.spin_turn(62)
-My_block.gyro_straight(75, 8)
+My_block.gyro_straight(75, 5)
+My_block.gyro_straight(50, 3.1)
 Robot.left_attachment.on_for_rotations(100, 1)
 My_block.gyro_straight(50, -2)
 My_block.spin_turn(90)
