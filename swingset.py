5ef6b29590f3ea694fa2021a27717611e7f651e1
Alejanda <alejandrapbonyuet@outlook.com>
2019-12-11 19:06:00 -0500

    tweaks

--- swingset.py
+++ swingset.py
@@ -13,9 +13,9 @@ Robot.steer_pair.on_for_rotations(0, 40, 1, False)
 Robot.steer_pair.on_for_rotations(0, 50, 0.5, False)
 """
 # go to swingset and release girl
-My_block.gyro_straight(50, 7.5)
+My_block.gyro_straight(50, 7.2)
 
-# back off swingset 
+# back off swingset  
 My_block.gyro_straight (30, -1)
 
 # smush against wall
@@ -24,13 +24,13 @@ My_block.wall_square(30)
 
 # drive to elevator
 My_block.gyro_straight(30, 1.62)
-My_block.spin_turn(50)
-My_block.gyro_straight(30, 1.75)
+My_block.spin_turn(52)
+My_block.gyro_straight(30, 2)
 
 # qwhack elevator
 Robot.left_attachment.on_for_rotations(35, 1)
 sleep(2)
 Robot.tank_pair.on_for_rotations(50, 50, -2.75)
-My_block.spin_turn(90)
+My_block.spin_turn(85)
 My_block.gyro_straight(50, -8)
 My_block.wall_square(30)
