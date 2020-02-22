dab65387e7b5896e4dd6c5d64657396eeaee3903
KDU <ken@goruck.com>
2020-02-22 13:27:14 -0500

    More tweaks for safety factor

--- swingset.py
+++ swingset.py
@@ -38,14 +38,14 @@ def swingset_mission():
     
     Robot.left_attachment.on_for_rotations(35, 1)
     sleep(0.5)
-    Robot.tank_pair.on_for_rotations(55, 55, -0.93)
+    Robot.tank_pair.on_for_rotations(55, 55, -0.85)
     Robot.left_attachment.on_for_rotations(-35, 0.95)
 
 
     # Do House Mission
     Robot.debug_print(Robot.gyro.compass_point)
 
-    My_block.spin_turn(95)
+    My_block.spin_turn(87)
 
     Robot.debug_print(Robot.gyro.compass_point)
 
@@ -53,29 +53,29 @@ def swingset_mission():
     
     Robot.debug_print(Robot.gyro.compass_point)
 
-    My_block.gyro_straight(30, 0.9)
+    My_block.gyro_straight(30, 0.71)
 
     Robot.debug_print(Robot.gyro.compass_point)
 
-    My_block.spin_turn(93)
+    My_block.spin_turn(95)
 
     Robot.debug_print(Robot.gyro.compass_point)
     
     Robot.steer_pair.on_for_rotations(0, -20, 0.3)
 
-    My_block.spin_turn(110)
+    My_block.spin_turn(90)
 
     Robot.debug_print(Robot.gyro.compass_point)
 
     Robot.steer_pair.on_for_rotations(0, -20, 0.3) 
 
-    My_block.spin_turn(90)
+    My_block.spin_turn(85)
 
     
     # Go Home.
 
 
     My_block.wall_square(100)
-    Robot.right_attachment.on_for_seconds(100, 4.2)
-
-    My_block.spin_turn(0)
+    Robot.right_attachment.on_for_rotations(100, 2.2)
+    sleep(0.4)
+    My_block.spin_turn(180)
